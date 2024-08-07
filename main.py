94944d0e0b1336bca0dfa5a5bc75cfb7e77712a1
fin <119663332+1f1n@users.noreply.github.com>
2024-08-07 03:35:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,74 +3,45 @@ import tls_client
 import json
 import re
 
-session = tls_client.Session(client_identifier="chrome_105")
-
-headers: dict = {
-    'accept': '*/*',
-    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-    'priority': 'u=1, i',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
-}
-
-cookies: dict = {
-     '_photon_ta': 'ADD YOUR PHOTON COOKIE HERE'
-}
-
 allData: dict = {}
-totalTraders: int = 0
 
-def getPairByCA(contractAddress: str) -> str:
-    getPairCA = session.get(f"https://photon-sol.tinyastro.io/en/lp/{contractAddress}", headers=headers).text
-    soup = BeautifulSoup(getPairCA, "html.parser")
-    pair = soup.find('a')['href'].replace("//", "/")
+totalTraders = 0
 
-    return pair
-
-def getPoolID(pair: str):
-    getPoolID = session.get(f"https://photon-sol.tinyastro.io/en/{pair}", headers=headers).text
-    soup = BeautifulSoup(getPoolID, "html.parser").find('script', string=re.compile(r'window\.taConfig.show')).string
-    poolMatch = re.search(r"'pool-id': (\d+)", soup)
-    pool_id = poolMatch.group(1)
-
-    return pool_id
+session = tls_client.Session(client_identifier="chrome_105")
 
 with open('tokens.txt', 'r') as fp:
     contractAddresses = fp.read().splitlines()
     print(f"[-] Dumping data..")
 
-
-
 for contractAddress in contractAddresses:
-        pair = getPairByCA(contractAddress)
-        pool_id = getPoolID(pair)
-        grabTopTradersURL = f"https://photon-sol.tinyastro.io/api/events/top_traders?order_by=timestamp&order_dir=desc&pool_id={pool_id}&page=1"
-        
-        response = session.get(grabTopTradersURL, headers=headers, cookies=cookies).json()
+        grabTopTradersURL = f"https://gmgn.ai/defi/quotation/v1/tokens/top_traders/sol/{contractAddress}?orderby=profit&direction=desc"
 
-        totalTraders += len(response['data'])
+        response = session.get(grabTopTradersURL).json()['data']
 
         allData[contractAddress] = {}
-        
-        for index, topTrader in enumerate(response['data']):
-            signer = topTrader['attributes']['signer']
-            boughtUsd = float(topTrader['attributes']['boughtUsd'])
-            soldUsd = float(topTrader['attributes']['soldUsd'])
-            unrealizedTokens = float(topTrader['attributes']['remainingTokens'])
-            
-            pumpFunBuyOrTokensTransferred = False
 
-            if boughtUsd == 0.0:
-                 pumpFunBuyOrTokensTransferred = True
-            
+        totalTraders = totalTraders + len(response)
 
-            allData[contractAddress][f"topTrader_{index + 1}"] = {
-                "pumpFunBuyOrTokensTransferred": pumpFunBuyOrTokensTransferred,
-                "maker": signer,
-                "amountBoughtUSD": boughtUsd,
-                "amountSoldUSD": soldUsd,
-                "unrealizedTokens": unrealizedTokens
-            }
-        
+        allData[contractAddress] = {}
+
+        for topTrader in response:
+              address = topTrader['address']
+              boughtUsd = f"${topTrader['total_cost']:,.2f}"
+              totalProfit = f"${topTrader['realized_profit']:,.2f}"
+              unrealizedProfit = f"${topTrader['unrealized_profit']:,.2f}"
+              multiplier = f"{topTrader['profit_change']:.2f}x" if topTrader['profit_change'] is not None else "?"
+              buys = f"{topTrader['buy_tx_count_cur']}"
+              sells = f"{topTrader['sell_tx_count_cur']}"
+
+              allData[contractAddress][address] = {
+                    "boughtUsd": boughtUsd,
+                    "totalProfit": totalProfit,
+                    "unrealizedProfit": unrealizedProfit,
+                    "multiplier": multiplier,
+                    "buys": buys,
+                    "sells": sells
+                }
+
 with open('top_traders.json', 'w') as fp:
     json.dump(allData, fp, indent=4)
-    print(f"[✅] Dumped {totalTraders * len(contractAddresses)} top traders for {len(contractAddresses)} tokens..")
+    print(f"[✅] Dumped {totalTraders} top traders for {len(contractAddresses)} tokens..")
